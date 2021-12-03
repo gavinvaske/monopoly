b38c1f71ce1fcd9c0e885a69e856bf86316cdf6d
Gavin Vaske <Gavin.Vaske@gmail.com>
2021-12-02 19:41:18 -0600

    wip: begin testing GameService

---
+++ test/services/GameService.spec.js
@@ -0,0 +1,41 @@
+const chance = require('chance').Chance();
+const GameService = require('../../application/services/GameService');
+const {GameModel} = require('../../application/models/GameModel');
+
+jest.mock('../../application/models/GameModel')
+
+afterEach(() => {
+    jest.clearAllMocks();
+});
+
+describe('game service', () => {
+    it('should find game if ID exists', async () => {
+        // given
+        const id = chance.integer();
+        GameModel.findById = jest.fn().mockResolvedValue({});
+
+        // when
+        const game = await GameService.findGameById(id);
+
+        // then
+        expect(GameModel.findById).toHaveBeenCalledTimes(1);
+        expect(GameModel.findById).toBeCalledWith(id);
+    });
+
+    it('should throw error if game is not found with ID', async () => {
+        // given
+        const id = chance.integer();
+        GameModel.findById = jest.fn().mockResolvedValue(null);
+        let errorMessage = '';
+
+        try {
+            // when
+            await GameService.findGameById(id);
+        } catch(e) {
+            errorMessage = e.message;
+        }
+
+        // then
+        expect(errorMessage).toEqual(`Could not find game using id ${id}`);
+    });
+});
