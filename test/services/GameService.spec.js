beb9824d2fcbfe6739715d56cfcd0fa0b54fc2f6
Gavin Vaske <Gavin.Vaske@gmail.com>
2021-12-03 19:09:47 -0600

    Finsih testing GameService

--- test/services/GameService.spec.js
+++ test/services/GameService.spec.js
@@ -38,4 +38,87 @@ describe('game service', () => {
         // then
         expect(errorMessage).toEqual(`Could not find game using id ${id}`);
     });
+
+    it('should increment the turn on the game', async () => {
+        // given
+        let game = {
+            turnCounter: 0,
+            save: jest.fn(),
+            players: createNPlayers(100),
+            board: {
+                boardSpaces: createNBoardSpaces(100)
+            }
+        }
+        const expectedTurnCounter = game.turnCounter + 1;
+
+        // when
+        game = await GameService.executeNextTurn(game);
+        
+        // then
+        expect(game.turnCounter).toBe(expectedTurnCounter);
+    });
+
+    it('should move the player', async () => {
+        // given
+        const playerStartingPosition = 0;
+        const player = {
+            boardPosition: playerStartingPosition
+        }
+        let game = {
+            turnCounter: 0,
+            save: jest.fn(),
+            players: [player],
+            board: {
+                boardSpaces: createNBoardSpaces(1000)
+            }
+        }
+
+        // when
+        game = await GameService.executeNextTurn(game);
+
+        // then
+        expect(game.players[0].boardPosition).toBeGreaterThan(playerStartingPosition);
+    });
+
+    it('should save the game', async () => {
+        // given
+        let game = {
+            turnCounter: 0,
+            save: jest.fn(),
+            players: createNPlayers(100),
+            board: {
+                boardSpaces: createNBoardSpaces(1000)
+            }
+        }
+
+        // when
+        await GameService.executeNextTurn(game);
+        
+        // then
+        expect(game.save).toHaveBeenCalledTimes(1);
+    });
+
+    function createNBoardSpaces(n) {
+        const boardPositions = [];
+
+        for (let i=0; i<n; i++) {
+            boardPositions.push({
+                positionOnBoard: i
+            })
+        }
+
+        return boardPositions;
+    }
+
+    function createNPlayers(n) {
+        const players = [];
+
+        for (let i=0; i<n; i++) {
+            players.push({
+                boardPosition: 0
+            })
+        }
+
+        return players;
+    }
 });
